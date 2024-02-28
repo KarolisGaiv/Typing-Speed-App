b9c51bcff93890335b7c997d0149cf1b80da3ff9
KarolisG <kgaivenis@gmail.com>
2024-02-28 10:27:15 +0200

    move api call function to other module

---
+++ api.js
@@ -0,0 +1,14 @@
+export async function getRandomQuote() {
+    try {
+        const res = await fetch("https://api.quotable.io/quotes/random?minLength=30&maxLength=100");
+        if (!res.ok) {
+            throw new Error(`Failed to fetch data ${res.status}`);
+        }
+        const data = await res.json();
+        const quote = data[0].content;
+        return quote;
+    } catch (err) {
+        console.log("Error while retreiving data: ", err);
+        return "Unable to retrieve quote. Try again later";
+    }
+}
